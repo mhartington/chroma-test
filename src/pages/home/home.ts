fdf29afe9ff40f4095cfdf5f7b98214aa22b08fa
mhartington <mikehartington@gmail.com>
2017-12-05 20:48:08 -0500

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import chroma from 'chroma-js';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  public value = '';
+  color = '';
   constructor(public navCtrl: NavController) {
-
+    this.color = chroma('#D4F880')
+      .darken()
+      .hex();
   }
-
 }
